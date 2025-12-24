7acb7a584dc7529e2b9e211ed2215441b6180099
Foez Ahmed <foez.official@gmail.com>
2025-12-24 23:53:53 +0600

    Add ddr3_tb testbench and include necessary files

--- hardware/filelist/testbench.f
+++ hardware/filelist/testbench.f
@@ -1,4 +1,11 @@
 -sv
 
+-i ${SOC}/include/vip
+
+// ${CORE_DDR3_CONTROLLER}/tb/ddr3_core_xc7/2048Mb_ddr3_parameters.vh
+${CORE_DDR3_CONTROLLER}/tb/ddr3_core_xc7/ddr3.v
+${HYPER_TITAN}/hardware/testbench/ddr3_tb.sv
+
 ${HYPER_TITAN}/hardware/testbench/hyper_titan_tb.sv
+
 ${HYPER_TITAN}/hardware/testbench/e_core_ss_tb.sv
