70ba2449be7b12930db4be0a95ed29d3933b1d1c
sikderAmit <amit.sder@gmail.com>
2026-01-21 01:29:45 +0600

    lint script added 	new file:   lint_script.tcl

---
+++ lint_script.tcl
@@ -0,0 +1,35 @@
+read file -type verilog axi2mem. sv
+# Started at 19:17:32, Completed at 19:17:39 (Elapsed time = 00:00:02.040)
+
+current goal lint/lint_rtl -alltop
+# Started at 19:18:30, Completed at 19:18:38 (Elapsed time = 00:00:04.080)
+
+run_goal
+# Started at 19:18:38, Completed at 19:18:58 (Elapsed time = 00:00:13.560)
+
+current goal lint/lint_rtl enhanced -alltop
+# Started at 19:18:58, Completed at 19:19:04 (Elapsed time = 00:00:04.259)
+
+run_goal
+# Started at 19:19:04, Completed at 19:19:19 (Elapsed time = 00:00:13.590)
+
+current goal lint/lint_turbo_rtl -alltop
+# Started at 19:19:19, Completed at 19:19:24 (Elapsed time = 00:00:04.150)
+
+run_goal
+# Started at 19:19:25, Completed at 19:19:39 (Elapsed time = 00:00:13.190)
+
+current goal lint/lint_functional_rtl -alltop
+# Started at 19:19:39, Completed at 19:19:43 (Elapsed time = 00:00:03.100)
+
+run_goal
+# Started at 19:19:43, Completed at 19:19:50 (Elapsed time = 00:00:05.149)
+
+current goal lint/lint_abstract -alltop
+# Started at 19:19:50, Completed at 19:19:52 (Elapsed time = 00:00:01.590)
+
+run_goal
+# Started at 19:19:52, Completed at 19:20:06 (Elapsed time = 00:00:13.179)
+
+current_goal lint/lint_rtl -alltop
+# Started at 19:20:06, Completed at 19:20:12 (Elapsed time = 00:00:04.329)
